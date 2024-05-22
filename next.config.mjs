77ff8a868023d73558d6e62ef28ea8ba7ccf26bd
egorxvc <egorxvc@gmail.com>
2024-05-22 06:34:00 +0300

    Prefix

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    assetPrefix: '/continental-next/'
+    assetPrefix: '/continental-next/',
+    basePath: '/continental-next',
 };
 
 export default nextConfig;
